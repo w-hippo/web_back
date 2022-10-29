c039e231e48346e3c9dc1798500ffcf41999e238
w-hippo <w.hippo.info@gmail.com>
2022-10-29 17:50:28 +0900

    first commit

---
+++ src/test/java/fdev/web_back/WebBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fdev.web_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
